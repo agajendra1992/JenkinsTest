00f98569eac5231dce78c4150a17b49d64b204ef
agajendra1992 <gajendra.akash02@gmail.com>
2020-03-22 16:54:46 +0530

    Added

---
+++ src/main/java/MainPackage_test/TestMain.java
@@ -0,0 +1,11 @@
+package MainPackage_test;
+
+import StringExample.Concat;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		new Concat().concattest();
+	}
+
+}
---
+++ src/main/java/StringExample/Concat.java
@@ -0,0 +1,12 @@
+package StringExample;
+
+public class Concat {
+
+	public  void concattest() {
+		String Sdata = "TEST";
+		String Sdata1 = "test";
+
+		System.out.println("Concat Data " + Sdata + "_" + Sdata1);
+	}
+
+}
